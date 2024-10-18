acb3514f65dad608d8a872293794b3bb7dd4c57a
ShinySSB <lilysandberg98@gmail.com>
2024-10-18 12:59:58 +0200

    struggling

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import os
 def main():
     switch_sd = get_directory('Please input the root of your SD card.')
 
-    switch_sd = switch_sd.rstrip("/\\")
-
     mods_folder = os.path.normpath(os.path.join(switch_sd, 'ultimate', 'mods'))
     skyline_plugins_folder = os.path.normpath(os.path.join(switch_sd, "atmosphere", "contents", "01006A800016E000", "romfs", "skyline", "plugins"))
 
@@ -16,11 +14,12 @@ def main():
     print('Mods folder: ' + mods_folder)
     print('Skyline plugins folder: ' + skyline_plugins_folder)
 
-    mods_list = list_directory_contents(mods_folder)
+    mods_list = build_mod_hierarchy(mods_folder)
     if mods_list:
         print("Mods found:")
         for mod in mods_list:
             print(mod.name)
+            list(scandir(mod))
     else:
         print("No Mods found")
 
@@ -36,6 +35,26 @@ def get_directory(prompt):
     directory = askdirectory()
     print("selected directory: " + directory)
     return directory
+
+def build_mod_hierarchy(mods_folder):
+    mod_hierarchy = {}
+    for root, dirs, files in os.walk(mods_folder):
+        current_level = mod_hierarchy
+        path_parts = root.split(os.sep)
+
+        for part in path_parts:
+            if part not in current_level:
+                current_level[part] = {}
+                current_level = current_level[part]
+
+        for file in files:
+            if parse_mod_filename(file):
+                current_level[file] = None
+
+    return mod_hierarchy
+
+def parse_mod_filename(filename):
+    if filename =
 
 def list_directory_contents(path):
     try:

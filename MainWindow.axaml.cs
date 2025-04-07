9522785a1b003d511d0dc716904633f8f48df846
ShinySSB <lilysandberg98@gmail.com>
2025-04-07 20:32:55 +0200

    initial

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Shinys_Ultimate_Mod_Manager;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
